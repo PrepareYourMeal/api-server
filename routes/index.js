87dad87f2ac3235e26ece4f11e60d27fbf10ac87
Benjamin Cai <benjamincaiyh@gmail.com>
2020-04-22 12:27:57 +0800

    Debugging admin route

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const configAPI = require('./api');
 
 module.exports = app => {
     app.use('/auth', authRoute);
-    app.use('/admin', passport.authenticate('jwt'), adminRoute);
+    app.use('/admin', passport.authenticate('jwt', { session: false }), adminRoute);
     configAPI(app);
     app.use('*', (req, res) => res.status(404).json({ msg: 'Page Not Found' }));
 };
